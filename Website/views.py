c03947b49dde99b4a77d71751a68ad8c1193c5f0
Sabastian-Ihekweme <155518932+Sabastian-Ihekweme@users.noreply.github.com>
2025-03-30 00:37:23 +0100

    Create more views

--- Website/views.py
+++ Website/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+#tutor views
+
 @views.route('/tutor-dashboard')
 def tutor_dashboard():
     return "<h2>Tutor Dashboard</h2>"
@@ -10,22 +12,70 @@ def tutor_dashboard():
 def tutor_details():
     return "<h2>Tutor Details</h2>"
 
-@views.route('/bookings')
-def bookings():
-    return "<h2>Bookings</h2>"
+@views.route('/tutor-bookings')
+def tutor_bookings():
+    return "<h2>Tutor Bookings</h2>"
 
-@views.route('/post')
-def post():
+@views.route('/tutor-make-post')
+def tutor_make_post():
     return "<h2>Post</h2>"
+
+@views.route('/tutor-chat')
+def tutor_chat():
+    return "<h2>Tutor Chat</h2>"
+
+@views.route('/tutor-live-session')
+def tutor_live_session():
+    return "<h2>Tutor Live Session</h2>"
+
+@views.route('/tutor-recent-chats')
+def tutor_recent_chats():
+    return "<h2>Tutor Recent Chats</h2>"
+
+@views.route('/tutor-view-post')
+def tutor_recent_chats():
+    return "<h2>Tutor Recent Chats</h2>"
+
+@views.route('/tutor-view-student')
+def tutor_view_student():
+    return "<h2>Tutor View Student</h2>"
+
+
+
+#student views
 
 @views.route('/student-dashboard')
 def student_dashboard():
     return "<h2>Student Dashboard</h2>"
 
-@views.route('/student-profile')
-def student_profile():
+@views.route('/student-details')
+def student_details():
     return "<h2>Student Profile</h2>"
 
-@views.route('/chat')
-def chat():
+@views.route('/student-chat')
+def student_chat():
     return "<h2>Chat<h2>"
+
+@views.route('/student-invitations')
+def student_invitations():
+    return "<h2>Student Invitations</h2>"
+
+@views.route('/student-live-session')
+def student_live_session():
+    return "<h2>Student Live Session</h2>"
+
+@views.route('/student-live-session-end')
+def student_live_session():
+    return "<h2>Student Live Session End</h2>"
+
+@views.route('/student-recent-chats')
+def student_live_session():
+    return "<h2>Student Recent Chats</h2>"
+
+@views.route('/student-view-materials')
+def student_view_materials():
+    return "<h2>Student View Materials</h2>"
+
+@views.route('/student-view-tutor')
+def student_view_tutor():
+    return "<h2>Student View Tutor</h2>"
