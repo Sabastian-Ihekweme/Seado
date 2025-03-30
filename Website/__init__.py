4bbcd47f6822ccb97616b4202d1588668b4365bf
Sabastian-Ihekweme <155518932+Sabastian-Ihekweme@users.noreply.github.com>
2025-03-30 01:34:17 +0100

    created database

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,18 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET KEY'] = 'airbus'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .auth import auth
     from .views import views
 
+
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(views, url_prefix='')
 
+    from .models import Tutor, Student
+    create_database(app)
+
+
     return app
+
+
+def create_database(app):
+    if not path.exists('Website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Database Created!')
 
 
    
---
+++ Website/models.py
@@ -0,0 +1,44 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+#Table for tutors
+class Tutor(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(150), nullable=False)
+    lastName = db.Column(db.String(150), nullable=False)
+    profilePicture = db.Column(db.String(255), nullable=True)
+    qualification = db.Column(db.String(150), nullable=False)
+    course = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+
+#Table for tutors
+class Student(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(150), nullable=False)
+    lastName = db.Column(db.String(150), nullable=False)
+    profilePicture = db.Column(db.String(255), nullable=True)
+    school = db.Column(db.String(150), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    Age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+
+#Table for posts
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.id'), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String(255), nullable=True)
+    video = db.Column(db.String(255), nullable=True)
+    document = db.Column(db.String(255), nullable=True)
+    thumbnail = db.Column(db.String(255), nullable=True)
+    subjectTag = db.Column(db.String(150), nullable=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    tutor = db.relationship('Tutor', backref=db.backref('posts', lazy=True))
